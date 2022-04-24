90baa279ce8c29cec90adae5fbb86d551dd3ec5a
NaTneSsIT <bestnnt2001@gmail.com>
2022-04-24 14:37:28 +0700

    TAN: update home

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,5 +1,113 @@
 @extends('admin.admin_master')
 @section('admin')
+    div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <?php
+                $reader = DB::table('readers')->count('*');
+                //                $book = DB::table('books')->where('status',0)->count('*');
+                $user = DB::table('users')->count('*');
+                $book = DB::table('books')->count('*');
+                $category = DB::table('categories')->count('*');
+                $borrow = DB::table('borrows')->count('*');
+                ?>
+                <div class="col-sm-6">
+                    <h1 class="m-0">Dashboard</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Dashboard v1</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
 
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+
+                    <!-- /.info-box -->
+
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>{!! $category !!}</h3>
+
+                            <p>Danh mục</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-list-alt"></i>
+                        </div>
+                        <a href="{{ route('category.view') }}" class="small-box-footer">Xem <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+
+                    <!-- small box -->
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3>{!! $book !!}</h3>
+
+                            <p>Các đầu sách</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-book"></i>
+                        </div>
+                        <a href="{{ route('book.view') }}" class="small-box-footer">Xem <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+
+                    <!-- /.info-box -->
+                    <!-- small box -->
+                    <div class="small-box bg-warning">
+                        <div class="inner">
+                            <h3>{!! $reader !!}</h3>
+
+                            <p>Bạn đọc</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-person-add"></i>
+                        </div>
+                        <a href="{{ route('reader.view') }}" class="small-box-footer">Xem <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-danger">
+                        <div class="inner">
+                            <h3>{!! $borrow !!}</h3>
+
+                            <p>Số lượt mượn sách</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-tasks"></i>
+                        </div>
+                        <a href="{{ route('borrow.view') }}" class="small-box-footer">Xem <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+            </div>
+            <!-- /.row -->
+            <!-- Main row -->
+            <div class="row">
+
+            </div>
+            <!-- /.row (main row) -->
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+    </div>
 
 @endsection
